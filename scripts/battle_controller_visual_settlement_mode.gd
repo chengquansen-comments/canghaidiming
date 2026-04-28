4f45ce1a85b7daddcb8e87aafbcef1ed3ed60432
chengquansen-comments <chengquansen@gmail.com>
2026-04-28 10:47:29 +0800

    wire: use preview position guard layer

--- scripts/battle_controller_visual_settlement_mode.gd
+++ scripts/battle_controller_visual_settlement_mode.gd
@@ -1,4 +1,4 @@
-extends "res://scripts/battle_controller_visual_presentation_mode_aware.gd"
+extends "res://scripts/battle_controller_visual_preview_position_guard.gd"
 
 # Thin runtime wrapper for story-battle selection and settlement-mode switching.
 #
