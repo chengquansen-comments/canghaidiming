4e22f993d9c5f040048504b59838fd1e5f5aaab9
chengquansen-comments <chengquansen@gmail.com>
2026-04-28 10:10:04 +0800

    wire: use mode-aware presentation layer

--- scripts/battle_controller_visual_settlement_mode.gd
+++ scripts/battle_controller_visual_settlement_mode.gd
@@ -1,4 +1,4 @@
-extends "res://scripts/battle_controller_visual_presentation_stepwise.gd"
+extends "res://scripts/battle_controller_visual_presentation_mode_aware.gd"
 
 # Thin runtime wrapper for story-battle selection and settlement-mode switching.
 #
